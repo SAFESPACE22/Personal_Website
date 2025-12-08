02f532625906a841e3dd0096e5bab7f9fa19230a
SAFESPACE22 <tylervuong03@gmail.com>
2025-12-08 16:59:46 -0600

    New commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,7 +35,7 @@ const Projects = () => {
                     </div>
                 </RevealOnScroll>
 
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
                     {projects.map((project, index) => (
                         <RevealOnScroll key={index}>
                             <div
